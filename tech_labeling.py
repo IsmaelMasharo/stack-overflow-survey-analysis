730e6051da8223cb0b871ccf043f14fd9e852b03
ismael <ismael.ubk@gmail.com>
2020-08-07 16:08:08 -0500

    technologies labeling based on its type

---
+++ tech_labeling.py
@@ -0,0 +1,74 @@
+tech_labeling = {
+    "javascript": "language",
+    "java": "language",
+    "sql": "language",
+    "c": "language",
+    "css": "language",
+    "php": "language",
+    "c++": "language",
+    "perl": "language",
+    "c#": "language",
+    "python": "language",
+    "nan": "",
+    "ruby": "language",
+    "objective-c": "language",
+    "html5": "language",
+    "jquery": "js",
+    "node js": "js",
+    "scala": "language",
+    "lamp": "tech",
+    "redis": "db",
+    "go": "language",
+    "sql server": "tech",
+    "cloud": "tech",
+    "mongodb": "db",
+    "android": "tech",
+    "arduino": "tech",
+    "raspberry pi": "tech",
+    "angular js": "js tech",
+    "windows phone": "tech",
+    "ios": "tech",
+    "sharepoint": "tech",
+    "wordpress": "tech",
+    "c++11": "language",
+    "coffeescript": "language",
+    "clojure": "language",
+    "salesforce": "tech",
+    "cordova": "tech",
+    "r": "language",
+    "swift": "language",
+    "hadoop": "tech", # apache
+    "visual basic": "language",
+    "spark": "tech",  # apache
+    "cassandra": "db",
+    "matlab": "language",
+    "dart": "language",
+    "f#": "language",
+    "haskell": "language",
+    "rust": "language",
+    "react js": "js tech",
+    "elixir": "language",
+    "erlang": "language",
+    "assembly": "language",
+    "vb.net": "language",
+    "typescript": "language", # js
+    "lua": "language",
+    "vba": "language",
+    "groovy": "language", # apache
+    "smalltalk": "language",
+    "visual basic 6": "language",
+    "common lisp": "language",
+    "julia": "language", # ds
+    "hack": "language", # fb, PHP dialect
+    "html": "language", # web
+    "bash": "language", # os
+    "shell": "language", # os
+    "kotlin": "language", # java, mobile
+    "cobol": "language",  # business oriented
+    "delphi": "language", # object pascal
+    "object pascal": "language",
+    "ocaml": "language",
+    "powershell": "tech",
+    "webassembly": "language",
+    "other(s):": "tech"
+}
