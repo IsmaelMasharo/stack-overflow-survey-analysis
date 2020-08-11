d8fed0abcf7b2982c73e5143d5fc6d930803fde2
ismael <ismael.ubk@gmail.com>
2020-08-11 14:56:57 -0500

    eda on job_satisfaction feature

---
+++ satisfaction_labeling.py
@@ -0,0 +1,53 @@
+satisfaction_scale = [
+    "Very satisfied",
+    "Slightly satisfied",
+    "Neither satisfied nor dissatisfied",
+    "Slightly dissatisfied",
+    "Very dissatisfied",
+]
+
+satisfaction_labeling = {    
+    "So happy it hurts": "Very satisfied",
+    "I enjoy going to work": "Slightly satisfied",
+    "It pays the bills": "Neither satisfied nor dissatisfied",
+    "I'm not happy in my job": "Slightly dissatisfied",
+    "FML": "Very dissatisfied",
+
+    "Love my job": "Very satisfied",
+    "I enjoy going to work": "Slightly satisfied",
+    "It's a paycheck": "Neither satisfied nor dissatisfied",
+    "I'm not happy in my job": "Slightly dissatisfied",
+    "hate my job": "Very dissatisfied",
+
+    "I love my job": "Very satisfied",
+    "I'm somewhat satisfied with my job": "Slightly satisfied",
+    "Neither satisfied nor dissatisfied": "Neither satisfied nor dissatisfied",
+    "I'm somewhat dissatisfied with my job": "Slightly dissatisfied",
+    "hate my job": "Very dissatisfied",
+
+    "10": "Very satisfied",
+    "9": "Very satisfied",
+    "8": "Slightly satisfied",
+    "7": "Slightly satisfied",
+    "6": "Slightly satisfied",
+    "5": "Neither satisfied nor dissatisfied",
+    "4": "Slightly dissatisfied",
+    "3": "Slightly dissatisfied",
+    "2": "Slightly dissatisfied",
+    "1": "Very dissatisfied",
+    "0": "Very dissatisfied",
+
+    "Extremely satisfied": "Very satisfied",
+    "Moderately satisfied": "Slightly satisfied",
+    "Slightly satisfied": "Slightly satisfied",
+    "Neither satisfied nor dissatisfied": "Neither satisfied nor dissatisfied",
+    "Slightly dissatisfied": "Slightly dissatisfied",
+    "Moderately dissatisfied": "Slightly dissatisfied",
+    "Extremely dissatisfied": "Very dissatisfied",
+
+    "Very satisfied": "Very satisfied",
+    "Slightly satisfied": "Slightly satisfied",
+    "Neither satisfied nor dissatisfied": "Neither satisfied nor dissatisfied",
+    "Slightly dissatisfied": "Slightly dissatisfied",
+    "Very dissatisfied": "Very dissatisfied",
+}
