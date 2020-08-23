6fea64c1f05166fe9bc2a4de11f4149cf502e161
ismael <ismael.ubk@gmail.com>
2020-08-23 13:05:43 -0500

    update job satisfaction labels imports

--- scripts/job_satisfaction_labeling.py
+++ scripts/job_satisfaction_labeling.py
@@ -1,22 +1,35 @@
-satisfaction_labeling = {    
+job_satisfaction_labeling = {    
+    # 2011
     "So happy it hurts": "Very satisfied",
     "I enjoy going to work": "Slightly satisfied",
     "It pays the bills": "Neither satisfied nor dissatisfied",
     "I'm not happy in my job": "Slightly dissatisfied",
     "FML": "Very dissatisfied",
-
+    # 2012
+    "Love my job": "Very satisfied",
+    "I enjoy going to work": "Slightly satisfied",
+    "Its a paycheck": "Neither satisfied nor dissatisfied",
+    "I'm not happy in my job": "Slightly dissatisfied",
+    "Hate my job": "Very dissatisfied",
+    # 2013
     "Love my job": "Very satisfied",
     "I enjoy going to work": "Slightly satisfied",
     "It's a paycheck": "Neither satisfied nor dissatisfied",
     "I'm not happy in my job": "Slightly dissatisfied",
-    "hate my job": "Very dissatisfied",
-
+    "Hate my job": "Very dissatisfied",
+    # 2015
     "I love my job": "Very satisfied",
     "I'm somewhat satisfied with my job": "Slightly satisfied",
-    "Neither satisfied nor dissatisfied": "Neither satisfied nor dissatisfied",
+    "I'm neither satisfied nor dissatisfied with my job": "Neither satisfied nor dissatisfied",
     "I'm somewhat dissatisfied with my job": "Slightly dissatisfied",
-    "hate my job": "Very dissatisfied",
-
+    "I hate my job": "Very dissatisfied",
+    # 2016
+    "I love my job": "Very satisfied",
+    "I'm somewhat satisfied with my job": "Slightly satisfied",
+    "I'm neither satisfied nor dissatisfied": "Neither satisfied nor dissatisfied",
+    "I'm somewhat dissatisfied with my job": "Slightly dissatisfied",
+    "I hate my job": "Very dissatisfied",
+    # 2017
     "10": "Very satisfied",
     "9": "Very satisfied",
     "8": "Slightly satisfied",
@@ -28,7 +41,7 @@ satisfaction_labeling = {
     "2": "Slightly dissatisfied",
     "1": "Very dissatisfied",
     "0": "Very dissatisfied",
-
+    # 2018
     "Extremely satisfied": "Very satisfied",
     "Moderately satisfied": "Slightly satisfied",
     "Slightly satisfied": "Slightly satisfied",
@@ -36,7 +49,13 @@ satisfaction_labeling = {
     "Slightly dissatisfied": "Slightly dissatisfied",
     "Moderately dissatisfied": "Slightly dissatisfied",
     "Extremely dissatisfied": "Very dissatisfied",
-
+    # 2019
+    "Very satisfied": "Very satisfied",
+    "Slightly satisfied": "Slightly satisfied",
+    "Neither satisfied nor dissatisfied": "Neither satisfied nor dissatisfied",
+    "Slightly dissatisfied": "Slightly dissatisfied",
+    "Very dissatisfied": "Very dissatisfied",
+    # 2020
     "Very satisfied": "Very satisfied",
     "Slightly satisfied": "Slightly satisfied",
     "Neither satisfied nor dissatisfied": "Neither satisfied nor dissatisfied",
@@ -44,7 +63,7 @@ satisfaction_labeling = {
     "Very dissatisfied": "Very dissatisfied",
 }
 
-satisfaction_scale = [
+job_satisfaction_scale = [
     "Very satisfied",
     "Slightly satisfied",
     "Neither satisfied nor dissatisfied",
